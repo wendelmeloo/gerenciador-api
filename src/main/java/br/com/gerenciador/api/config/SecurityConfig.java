426fe15bf503360a182eda6b0b46870ee1bc3f06
wendelmeloo <wendelmelo21@gmail.com>
2025-04-05 14:05:19 -0300

    url vercel

--- src/main/java/br/com/gerenciador/api/config/SecurityConfig.java
+++ src/main/java/br/com/gerenciador/api/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://gp-prof-front.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://front-gerenciador-turma-03.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
